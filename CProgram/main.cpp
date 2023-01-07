fce6c7d7c330918fae6b2c9d6ecf85e15a00c434
MuxinZhou <1900012994@pku.edu.cn>
2023-01-07 10:32:23 +0800

    Initial Commit

---
+++ CProgram/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CProgram
+//
+//  Created by ZhouMuxin on 2023/1/7.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World\n";
+    return 0;
+}
